fd2d3a781d12676afeb0370338d650ff45987bb3
platolinc <2686550623@qq.com>
2024-04-05 15:24:16 +0800

    feat: 处理me接口loading和error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 // emotion js 使用变量自定义样式
 export const Row = styled.div<{
@@ -22,3 +24,19 @@ export const Row = styled.div<{
           : undefined};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const FullPageLoading = () => <FullPage>
+  <Spin size={"large"}/>
+</FullPage> 
+
+export const FullPageErrorFallback = ({error}: {error: Error | null}) => <FullPage>
+  <DevTools></DevTools>
+  <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+</FullPage> 
