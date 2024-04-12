a1afd73a921a29a8618191b95bd1e3bfabe7c7ae
platolinc <2686550623@qq.com>
2024-04-13 00:03:34 +0800

    feat: 看板与任务的创建、编辑、删除

--- src/screens/kanban/index.jsx
+++ src/screens/kanban/index.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import { useDocumentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
-import { useKanbanSearchParams, useProjectInUrl } from "screens/kanban/util";
+import { useKanbanSearchParams, useProjectInUrl, useTasksSearchParams } from "screens/kanban/util";
 import { KanbanColumn } from "screens/kanban/kanban-column";
 import styled from "@emotion/styled";
 import { SearchPanel } from "./search-panel";
 import { ScreenContainer } from "components/lib";
-
+import {Spin} from 'antd'
+import { useTasks } from "../../utils/task";
+import {CreateKanban} from "./create-kanban"
+import { TaskModal } from "./task-modal";
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
 
   const { data: currentProject } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(useKanbanSearchParams());
+  const {isLoading: taskIsLoading} = useTasks(useTasksSearchParams())
+  const isLoading = taskIsLoading || kanbanIsLoading
   return (
     <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
+      {isLoading ? <Spin size="large"/> : <ColumnsContainer>
         {kanbans?.map(
           kanban => <KanbanColumn kanban={kanban} key={kanban.id} />
         )
         }
-      </ColumnsContainer> 
+        <CreateKanban />
+      </ColumnsContainer>}
+      <TaskModal/>
     </ScreenContainer>
   );
 };
@@ -30,4 +37,24 @@ const ColumnsContainer = styled.div`
   display: flex;
   overflow-x: scroll;
   flex: 1;
+
+  ::-webkit-scrollbar {
+    height: 1rem;
+    border-radius: 1rem;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  /*定义滚动条轨道
+     内阴影+圆角*/
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    border-radius: 1rem;
+    background-color: #f5f5f5;
+  }
+  /*定义滑块
+     内阴影+圆角*/
+  ::-webkit-scrollbar-thumb {
+    border-radius: 1rem;
+    -webkit-box-shadow: inset 0 0 2px rgba(0, 0, 0, 0.1);
+    background-color: rgba(0, 0, 0, 0.2);
+  }
 `;
--- src/screens/kanban/util.ts
+++ src/screens/kanban/util.ts
@@ -1,6 +1,7 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { useLocation } from "react-router";
 import { useProject } from "utils/project";
+import { useTask } from "utils/task";
 import { useUrlQueryParam } from "utils/url";
 // import { useKanbans } from "utils/kanban";
 // import { useTasks } from "utils/task";
@@ -38,3 +39,27 @@ export const useTasksSearchParams = () => {
 }
 
 export const useTasksQueryKey = () => ['tasks', useTasksSearchParams]
+
+export const useTasksModal = () => {
+  const [{ editingTaskId }, setEditingTaskId] = useUrlQueryParam([
+    "editingTaskId",
+  ]);
+  const { data: editingTask, isLoading } = useTask(Number(editingTaskId));
+  const startEdit = useCallback(
+    (id: number) => {
+      setEditingTaskId({ editingTaskId: id });
+    },
+    [setEditingTaskId],
+  );
+  const close = useCallback(() => {
+    setEditingTaskId({ editingTaskId: "" });
+  }, [setEditingTaskId]);
+
+  return {
+    editingTaskId,
+    editingTask,
+    startEdit,
+    close,
+    isLoading,
+  };
+};
